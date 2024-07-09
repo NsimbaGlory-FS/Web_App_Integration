fb72a40bce425b93e7f95b1a0199c601051bb525
Glory Dounce <glorynsimba@Glorys-MBP.lan>
2024-07-09 12:53:39 -0400

    push

--- index.test.js
+++ index.test.js
@@ -1,6 +1,6 @@
 describe("Obscure a credit card number function Test", () => {
   test("As a user I want to test positive for a credit card function.", () => {
-    expect(creditCardObscure("123456789012"), "########9012").toEqual(8);
+    expect(creditCardObscure("123456789012"), "########9012").toEqual(9);
   });
 });
 
