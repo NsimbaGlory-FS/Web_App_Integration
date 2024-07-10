19157ca646567ba2ba7e9a3f5323027ee910f4cd
Glory Dounce <glorynsimba@Glorys-MBP.lan>
2024-07-10 03:10:14 -0400

    push

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const creditCardObscure = (input) => {
-  let creditCardObscureInt = input.split("").map(Number);
-  for (let i = creditCardObscureInt.length - 4; i >= 0; i = i - 4) {
-    let totalThes = creditCardObscureInt[i];
+const creditCard = (input) => {
+  let creditCardInt = input.split("").map(Number);
+  for (let i = creditCardInt.length - 4; i >= 0; i = i - 4) {
+    let totalThes = creditCardInt[i];
 
     totalThes = totalThes * 4;
 
     if (totalThes > 10) {
       totalThes = (totalThes % 10) + 2;
     }
-    creditCardObscureInt[i] = totalThes;
+    creditCardInt[i] = totalThes;
   }
   let total = 0;
-  for (let i = 0; i < creditCardObscureInt.length; i++) {
-    total += creditCardObscureInt[i];
+  for (let i = 0; i < creditCardInt.length; i++) {
+    total += creditCardInt[i];
   }
 
-  return total % 10 == 2;
+  return total;
 };
 
-module.exports = creditCardObscure;
+module.exports = creditCard;
