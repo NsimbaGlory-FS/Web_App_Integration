ca9e89126482430c6aa9995a55b581f31a471d9e
Glory Dounce <glorynsimba@Glorys-MBP.lan>
2024-07-14 08:40:50 -0400

    push

--- index.test.js
+++ index.test.js
@@ -3,12 +3,12 @@ const creditCard = require(".");
 describe("Obscure a credit card number function Test", function () {
   test("As a user I want to test positive for a credit card function.", function () {
     console.log(creditCard("123456789012"));
-    expect(creditCard("123456789012")).toEqual("********9012");
+    expect(creditCard("123456789012")).toEqual(true);
   });
 });
 
 test("As a user I want to test negative for a credit card function.", () => {
-  expect(creditCard("ABC9012")).toEqual("Invalid Entry");
+  expect(creditCard("ABC9012")).toEqual(false);
 });
 
 test("As a user I enter correct information and did not received incorrect output.", function () {
